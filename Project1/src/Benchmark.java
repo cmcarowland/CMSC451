a42f7d6559bed320090777ac9b8955b7fe258346
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2026-07-04 00:20:53 +0000

    feat: Added UnsortedException to the benchmark application

--- Project1/src/Benchmark.java
+++ Project1/src/Benchmark.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 class Benchmark
 {
-    static public void main(String[] args)
+    static public void main(String[] args) throws UnsortedException
     {
         SortAbstract absSort = new SelectionSort();
         int[] array = new int[]{100,99,98,97,96,95,94,93,92,91,90,89,88,87,86,85,84,83,82,81,80,79,78,77,76,75,74,73,72,71,70,69,68,67,66,65,64,63,62,61,60,59,58,57,56,55,54,53,52,51,50,49,48,47,46,45,44,43,42,41,40,39,38,37,36,35,34,33,32,31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0};
@@ -10,17 +10,20 @@ class Benchmark
         int[] array3 = array.clone();
 
         absSort.sort(array);
-        System.out.println("Sorted Array: " + validateSort(array) + " Count: " + absSort.getCount() + " Time: " + absSort.getTime());
+        System.out.println("Count: " + absSort.getCount() + " Time: " + absSort.getTime());
+        validateSort(array);
         System.out.println();
 
         absSort = new InsertionSort();
         absSort.sort(array2);
-        System.out.println("Sorted Array: " + validateSort(array2) + " Count: " + absSort.getCount() + " Time: " + absSort.getTime());
+        System.out.println("Count: " + absSort.getCount() + " Time: " + absSort.getTime());
+        validateSort(array2);
         System.out.println();
 
         absSort = new QuickSort();
         absSort.sort(array3);
-        System.out.println("Sorted Array: " + validateSort(array3) + " Count: " + absSort.getCount() + " Time: " + absSort.getTime());
+        System.out.println("Count: " + absSort.getCount() + " Time: " + absSort.getTime());
+        validateSort(array3);
         System.out.println();
 
         int[] shuffled = new int[100];
@@ -33,17 +36,20 @@ class Benchmark
 
         absSort = new SelectionSort();
         absSort.sort(shuffled);
-        System.out.println("Sorted Array: " + validateSort(shuffled) + " Count: " + absSort.getCount() + " Time: " + absSort.getTime());
+        System.out.println("Count: " + absSort.getCount() + " Time: " + absSort.getTime());
+        validateSort(shuffled);
         System.out.println();
 
         absSort = new InsertionSort();
         absSort.sort(shuffled2);
-        System.out.println("Sorted Array: " + validateSort(shuffled2) + " Count: " + absSort.getCount() + " Time: " + absSort.getTime());
+        System.out.println("Count: " + absSort.getCount() + " Time: " + absSort.getTime());
+        validateSort(shuffled2);
         System.out.println();
 
         absSort = new QuickSort();
         absSort.sort(shuffled3);
-        System.out.println("Sorted Array: " + validateSort(shuffled3) + " Count: " + absSort.getCount() + " Time: " + absSort.getTime());
+        System.out.println("Count: " + absSort.getCount() + " Time: " + absSort.getTime());
+        validateSort(shuffled3);
         System.out.println();
     }
 
@@ -59,18 +65,16 @@ class Benchmark
         }
     }
 
-    static public boolean validateSort(int[] array)
+    static public void validateSort(int[] array) throws UnsortedException
     {
         for (int i = 0; i < array.length - 1; i++)
         {
             if (array[i] > array[i + 1])
             {
                 System.out.println("Array is not sorted");
-                return false;
+                throw new UnsortedException("Array is not sorted");
             }
         }
         System.out.println("Array is sorted");
-
-        return true;
     }
 }
