1323246abebb0ddef1afb6a2bddcb9793b79b89f
Raymond Rowland <82390744+cmcarowland@users.noreply.github.com>
2026-07-04 00:05:24 +0000

    feat: Added Quicksort to see how its numbers look against the PDF

---
+++ Project1/src/QuickSort.java
@@ -0,0 +1,57 @@
+public class QuickSort extends SortAbstract{
+    @Override
+    public void sort(int[] array) {
+        System.out.println("Quick Sort");
+        startSort();
+        quickSort(array, 0, array.length - 1);
+        endSort();
+    }
+
+    // partition function
+    int partition(int[] arr, int low, int high) {
+        
+        // choose the pivot
+        int pivot = arr[high];
+        
+        // index of smaller element and indicates 
+        // the right position of pivot found so far
+        int i = low - 1;
+
+        // traverse arr[low..high] and move all smaller
+        // elements to the left side. Elements from low to 
+        // i are smaller after every iteration
+        for (int j = low; j <= high - 1; j++) {
+            addToCount();
+            if (arr[j] < pivot) {
+                i++;
+                swap(arr, i, j);
+            }
+        }
+        
+        // Move pivot after smaller elements and
+        // return its position
+        swap(arr, i + 1, high);  
+        return i + 1;
+    }
+
+    // swap function
+    void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    // the QuickSort function implementation
+    void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            
+            // pi is the partition return index of pivot
+            int pi = partition(arr, low, high);
+
+            // recursion calls for smaller elements
+            // and greater or equals elements
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+}
